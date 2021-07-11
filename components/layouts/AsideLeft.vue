86adee03bccc4096c1c5de615de697b3760ffa8d
gibaek-lee <bbsnake10@gmail.com>
2021-07-11 23:39:42 +0900

    refactor: default 레이아웃 요소 컴포넌트 분리

---
+++ components/layouts/AsideLeft.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-navigation-drawer
+    :value="value"
+    :mini-variant="miniVariant"
+    :clipped="clipped"
+    fixed
+    app
+  >
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    },
+    miniVariant: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    clipped: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  setup () {
+  },
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Music Visualizer',
+          to: '/music-visualizer'
+        },
+        {
+          icon: 'mdi-lifebuoy',
+          title: 'Haunted House',
+          to: '/haunted-house'
+        }
+      ]
+    }
+  }
+})
+</script>
---
+++ components/layouts/DefaultHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app-bar
+    :clipped-left="clipped"
+    fixed
+    app
+  >
+    <v-app-bar-nav-icon @click.stop="$emit('onDrawer')" />
+    <v-btn
+      icon
+      @click.stop="$emit('onMiniVariant')"
+    >
+      <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+    </v-btn>
+    <v-btn
+      icon
+      @click.stop="$emit('onClipped')"
+    >
+      <v-icon>mdi-application</v-icon>
+    </v-btn>
+    <v-btn
+      icon
+      @click.stop="$emit('onFixed')"
+    >
+      <v-icon>mdi-minus</v-icon>
+    </v-btn>
+    <v-toolbar-title v-text="titleDescription" />
+    <v-spacer />
+    <v-btn
+      icon
+      @click.stop="$emit('onRightDrawer')"
+    >
+      <v-icon>mdi-menu</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: {
+    clipped: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    miniVariant: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  emits: ['onDrawer', 'onClipped', 'onMiniVariant', 'onFixed', 'onRightDrawer'],
+  setup () {
+  },
+  computed: {
+    titleDescription () {
+      return 'Playground Three.js'
+    }
+  }
+})
+</script>
